014a16fc22e8142266795db10465f3a63f4e7c05
Tyler Smith <tyler.smith@pteraforce.com>
2016-11-24 19:57:02 -0800

    Add dummy funtion to test JS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 'use strict';
 
-let test = 'test';
+let canvas = document.getElementById('gameViewport');
+let context = canvas.getContext('2d');
+
+context.strokeStyle = '#F00';
+
+console.log('going to stroke');
+
+context.beginPath();
+context.arc(100, 100, 50, 0, 2 * Math.PI);
+context.stroke();
